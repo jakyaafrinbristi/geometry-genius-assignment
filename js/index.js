cea6e04f402d283003eca2ce16d6d467158bc183
jakyaafrinbristi <jakyaafrinb@gmail.com>
2023-10-24 07:17:06 +0600

    event listen js added

---
+++ js/index.js
@@ -0,0 +1,70 @@
+let serial=0;
+// first-card
+document.getElementById("triangle-button").addEventListener('click',function(){
+    serial +=1;
+    const geometryName =document.getElementById('triangle-card').innerText;
+    const baseInput =document.getElementById('triangle-first-input').value ;
+    const heightInput = document.getElementById('triangle-second-input').value ;
+    const triangleFormula = 0.5*baseInput*heightInput; 
+    displayDataGeo(geometryName,triangleFormula);
+      
+})
+// second-card
+document.getElementById("rectangle-button").addEventListener('click',function(){
+    serial +=1;
+    const geometryNameTwo =document.getElementById('rectangle-card').innerText;
+    const widthInput =document.getElementById('rectangle-first-input').value ;
+    const lengthInput = document.getElementById('rectangle-second-input').value ;
+    const rectangleFormula = widthInput*lengthInput; 
+    displayDataGeo(geometryNameTwo,rectangleFormula);
+    
+    
+})
+// third-card
+document.getElementById("parallelogram-button").addEventListener('click',function(){
+    serial +=1;
+    // const geometryNameThree =document.getElementById('parallelogram-card').innerText;
+    // const baseInput =document.getElementById('parallelogram-value-one').innerText;
+    // const heightInput = document.getElementById('parallelogram-value-two').innerText ;
+    const gd =geometryGetData("parallelogram-card","parallelogram-value-one","parallelogram-value-two")
+    const parallelogramFormula =parseFloat(gd.geometryFirstInput)*parseFloat(gd.geometrySecondInput); 
+    displayDataGeo(gd.geometryName,parallelogramFormula);
+    
+    
+})
+// fourth-card
+document.getElementById("rhombus-button").addEventListener('click',function(){
+    serial +=1;
+    // const geometryNameFour =document.getElementById('rhombus-card').innerText;
+    // const diagonalFirstInput =document.getElementById('rhombus-value-one').innerText;
+    // const diagonalSecondInput = document.getElementById('rhombus-value-two').innerText ;
+    const gd =geometryGetData("rhombus-card","rhombus-value-one","rhombus-value-two");
+    const rhombusFormula = 0.5*parseFloat(gd.geometryFirstInput)*parseFloat(gd.geometrySecondInput);
+    displayDataGeo(gd.geometryName,rhombusFormula );
+    
+    
+})
+// fifth-card
+document.getElementById("pentagon-button").addEventListener('click',function(){
+    serial +=1;
+    // const geometryNameFive =document.getElementById('pentagon-card').innerText;
+    // const pentagonFirstInput =document.getElementById('pentagon-first-value').innerText;
+    // const pentagonSecondInput = document.getElementById('pentagon-second-value').innerText ;
+    const gd =geometryGetData("pentagon-card","pentagon-first-value","pentagon-second-value");
+    const pentagonFormula = 0.5*parseFloat(gd.geometryFirstInput)*parseFloat(gd.geometrySecondInput);
+    displayDataGeo(gd.geometryName,pentagonFormula );
+    
+    
+})
+// sixth-card
+document.getElementById("ellipse-button").addEventListener('click',function(){
+    serial +=1;
+    // const geometryNameSix =document.getElementById('ellipse-card').innerText;
+    // const ellipseFirstInput =document.getElementById('ellipse-first-value').innerText;
+    // const ellipseSecondInput = document.getElementById('ellipse-second-value').innerText ;
+    const gd =geometryGetData("ellipse-card","ellipse-first-value","ellipse-second-value");
+    const ellipseFormula = 3.1416*parseFloat(gd.geometryFirstInput)*parseFloat(gd.geometrySecondInput);  
+    displayDataGeo(gd.geometryName,ellipseFormula );
+    
+    
+})
